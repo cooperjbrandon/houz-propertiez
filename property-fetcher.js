ead9279d473fc1f1c7ea022ed8bdad5d278b3412
Brandon Cooper <cooperjbrandon@gmail.com>
2015-03-26 21:24:31 -0700

    initialize service for fetching property info

---
+++ property-fetcher.js
@@ -0,0 +1,47 @@
+var http, clc, rabbit, beginSetup, handlePropertyInfo;
+
+http = require('http');
+clc = require('cli-color');
+rabbit = require('./rabbit-management');
+
+beginSetup = rabbit.beginSetup;
+handlePropertyInfo = rabbit.handlePropertyInfo;
+
+var beginFetchOfProperty = function(message, headers, deliveryInfo, messageObject) {
+	fetchProperty(message.zipid);
+};
+
+var fetchProperty = function(zipid) {
+	var url = 'http://www.zillow.com/homes/'+zipid+'_zpid/';
+	console.log(clc.green('SENDING REQUEST: zipid '+ zipid));
+	http.get(url, function(result) {
+		var html = '';
+		console.log(clc.black.bgWhite('STATUS: ' + result.statusCode));
+		result.on('data', function(chunk) {
+			html += new Buffer(chunk).toString('utf8');
+		});
+		result.on('end', function() {
+			var propertyInfo = parsePropertyInfo(html);
+			handlePropertyInfo(propertyInfo);
+		});
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+};
+
+var parsePropertyInfo = function(html) {
+	var startingIndex = html.indexOf('"minibubble"');
+	var endingIndex = html.indexOf('"enrollmentId"');
+	while (html[endingIndex] !== '}') {
+		endingIndex--; //keeping going untill hitting a }
+	}
+	var propertyInfoStr = html.slice(startingIndex, endingIndex+1);
+	try {
+		var propertyInfoObj = JSON.parse('{'+propertyInfoStr+'}');
+		return propertyInfoObj.minibubble;
+	} catch(err) {
+		console.log('Error parsing JSON: '+ err);
+	}
+};
+
+beginSetup(beginFetchOfProperty);
---
+++ rabbit-management.js
@@ -0,0 +1,67 @@
+var amqp, clc, moment, beginFetchOfProperty,
+		connection, routingKey,
+		queue, queueName, queueConnected,
+		exchange, exchangeName, exchangeConnected;
+
+amqp = require('amqp');
+clc = require('cli-color');
+moment = require('moment');
+
+exchangeName = 'houz-exchange';
+queueName = 'houz-queue-getzipid';
+routingKey = 'zipids';
+
+var beginSetup = function(beginFetch) {
+	beginFetchOfProperty = beginFetch;
+	connection = amqp.createConnection();
+	connection.on('ready', connectToQueueAndExchange);
+};
+
+var connectToQueueAndExchange = function() {
+	console.log(clc.blue('The connection is ready'));
+	connectToQueue();
+	connectToExchange();
+};
+
+var connectToExchange = function() {
+	exchange = connection.exchange(exchangeName, {autoDelete: false}); //connect to exchange
+	exchange.on('open', function() { queueOrExchangeReady('exchange'); });
+}
+
+var connectToQueue = function() {
+	queue = connection.queue(queueName); //connect to queue
+	queue.on('open', bindQueueToExchange);
+};
+
+var bindQueueToExchange = function() {
+	console.log(clc.blue('The queue "' + queue.name + '" is ready'));
+	queue.bind(exchangeName, routingKey); //bind to exchange w/ routingKey (most likely already done; this is just incase)
+	queue.on('queueBindOk', function() { queueOrExchangeReady('queue'); });
+};
+
+var queueOrExchangeReady = function(type) {
+	if (type === 'exchange') {
+		console.log(clc.bgBlueBright('The exchange "' +exchange.name+ '" is ready'));
+		exchangeConnected = true;
+	} else if (type === 'queue') {
+		console.log(clc.blue('The queue "' +queue.name+ '" is bound to the exchange "' +exchangeName+ '" with the routing key "' +routingKey+ '"'));
+		queueConnected = true;
+	}
+	if (exchangeConnected && queueConnected) { subscribeToQueue(); }
+};
+
+var subscribeToQueue = function() {
+	queue.subscribe({ack: true}, messageReceiver); //subscribe to queue
+};
+
+var messageReceiver = function(message, headers, deliveryInfo, messageObject) {
+	console.log(clc.yellow('Message received: Zip ID ' + message.zipid + ' at ' + moment().format('MMMM Do YYYY, h:mm:ss a')));
+	beginFetchOfProperty(message, headers, deliveryInfo, messageObject);
+};
+
+var handlePropertyInfo = function(propertyinfo) {
+	exchange.publish('propertyinfo', { propertyinfo: propertyinfo }); //routingKey, message
+};
+
+module.exports.beginSetup = beginSetup;
+module.exports.handlePropertyInfo = handlePropertyInfo;
