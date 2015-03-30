4be94d80e3fa5f461169d15804a0ca96394a4770
Brandon Cooper <cooperjbrandon@gmail.com>
2015-03-29 18:41:27 -0700

    formatting property info object before pushing to exchange

--- property-fetcher.js
+++ property-fetcher.js
@@ -35,11 +35,11 @@ var parsePropertyInfo = function(html) {
 	while (html[endingIndex] !== '}') {
 		endingIndex--; //keeping going untill hitting a }
 	}
-	var propertyInfoStr = html.slice(startingIndex, endingIndex+1);
-	var sanitizedPropertyInfo = sanitizePropertyInfo(propertyInfoStr);
 	try {
-		var propertyInfoObj = JSON.parse('{'+sanitizedPropertyInfo+'}');
-		return propertyInfoObj.minibubble;
+		var propertyInfoStr = html.slice(startingIndex, endingIndex+1);
+		// var sanitizedPropertyInfo = sanitizePropertyInfo(propertyInfoStr);
+		var propertyInfoObj = JSON.parse('{'+propertyInfoStr+'}');
+		return formatPropertyInfo(propertyInfoObj);
 	} catch(err) {
 		console.log('Error parsing JSON: '+ err);
 		console.log(propertyInfoStr);
@@ -48,6 +48,62 @@ var parsePropertyInfo = function(html) {
 
 var sanitizePropertyInfo = function(propInfo) {
 	return propInfo.replace('"lat":/', '"lat":').replace('"lng":/', '"lng":');
+};
+
+var formatPropertyInfo = function(propertyInfoObj) {
+	/* input object:
+	{
+		"minibubble": {
+		"id":63065270,
+		"statusType":1,
+		"lat":37326918,
+		"lng":-121905723,
+		"data": {
+			"bed":3,
+			"miniBubbleType":1,
+			"image":"http:\\/\\/photos2.zillowstatic.com\\/p_a\\/IStgkrhj0yauk21000000000.jpg",
+			"sqft":1463,
+			"label":"$700K",
+			"isPropertyTypeVacantLand":false,
+			"datasize":8,
+			"title":"$700K",
+			"bath":3.0
+		} 
+	}
+	*/
+	return {
+		zipid: propertyInfoObj.minibubble.id,
+		location: {
+			type: 'Point',
+			coordinates: [propertyInfoObj.minibubble.lng/100000, propertyInfoObj.minibubble.lat/100000]
+		}
+		bed: propertyInfoObj.minibubble.data.bed,
+		bath: propertyInfoObj.minibubble.data.bath,
+		image: propertyInfoObj.minibubble.data.image,
+		sqft: propertyInfoObj.minibubble.data.sqft,
+		price: determinePrice(propertyInfoObj.minibubble.data.title)
+	};
+
+	/* output object:
+	{
+		zipid:63065270,
+		location: {
+			type: "Point",
+			coordinates: [-121.905723, 37.326918] //lng, lat
+		},
+		bed: 3,
+		bath: 3,
+		image: "http:\\/\\/photos2.zillowstatic.com\\/p_a\\/IStgkrhj0yauk21000000000.jpg",
+		sqft: 1463,
+		price: 700000
+	}
+	*/
+};
+
+var determinePrice = function(priceStr) {
+	// either '700K' or '2.79M' (M or K)
+	var priceNum = JSON.parse(priceStr.slice(0,-1));
+	return priceStr.slice(-1 === 'K') ? priceNum * 1000 : priceNum * 1000000;
 };
 
 beginSetup(beginFetchOfProperty);
